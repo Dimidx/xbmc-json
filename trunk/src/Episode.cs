a5ea35c88be6ef316478e28211e4f3c49b1c5fbb
tyler@digitalreality.co.nz <tyler@digitalreality.co.nz@d9727985-65ab-e424-f487-56934fdd5c17>
2010-07-23 21:15:08 +0000

    Added classes for each media type, started to update functions to return lists of media types instead of json objects.

---
+++ trunk/src/Episode.cs
@@ -0,0 +1,59 @@
+﻿using Jayrock.Json;
+using System;
+
+namespace XbmcJson
+{
+    public class Episode
+    {
+        private int _id;
+        private string File;
+        private string Label;
+        private string Thumbnail;
+
+        public int id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public string file
+        {
+            get
+            {
+                return File;
+            }
+        }
+
+        public string label
+        {
+            get
+            {
+                return Label;
+            }
+        }
+
+        public string thumbnail
+        {
+            get
+            {
+                return Thumbnail;
+            }
+        }
+
+        public Episode(int id, string file, string label, string thumbnail)
+        {
+            _id = id;
+            File = file;
+            Label = label;
+            Thumbnail = thumbnail;
+        }
+
+        public static Episode EpisodeFromJsonObject(JsonObject item)
+        {
+            Episode e = new Episode(Convert.ToInt32(item["episodeid"]), item["file"].ToString(), item["label"].ToString(), (item["thumbnail"] != null) ? item["thumbnail"].ToString() : "");
+            return e;
+        }
+    }
+}
---
+++ trunk/src/TvShow.cs
@@ -0,0 +1,59 @@
+﻿using Jayrock.Json;
+using System;
+
+namespace XbmcJson
+{
+    public class TvShow
+    {
+        private int _id;
+        private string File;
+        private string Label;
+        private string Thumbnail;
+
+        public int id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public string file
+        {
+            get
+            {
+                return File;
+            }
+        }
+
+        public string label
+        {
+            get
+            {
+                return Label;
+            }
+        }
+
+        public string thumbnail
+        {
+            get
+            {
+                return Thumbnail;
+            }
+        }
+
+        public TvShow(int id, string file, string label, string thumbnail)
+        {
+            _id = id;
+            File = file;
+            Label = label;
+            Thumbnail = thumbnail;
+        }
+
+        public static TvShow TvShowFromJsonObject(JsonObject item)
+        {
+            TvShow e = new TvShow(Convert.ToInt32(item["tvshowid"]), item["file"].ToString(), item["label"].ToString(), (item["thumbnail"] != null) ? item["thumbnail"].ToString() : "");
+            return e;
+        }
+    }
+}
