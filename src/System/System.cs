3600f51767e318d4d8ebf735d51e4693d0b2cbfa
tyler@digitalreality.co.nz <tyler@digitalreality.co.nz@d9727985-65ab-e424-f487-56934fdd5c17>
2010-07-27 02:16:46 +0000

    Fixed bug in JsonClient.cs where args that weren't JObject keyvalue pairs would cause the call to to die quietly. Started adding "interesting" system label methods.

--- src/System/System.cs
+++ src/System/System.cs
@@ -1,4 +1,9 @@
-﻿namespace XbmcJson
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace XbmcJson
 {
    public class XbmcSystem
     {
@@ -9,16 +14,50 @@
             Client = client;
         }
 
-       //Not implemented in xbmc yet so can't test
-      /*  public object GetInfoLabels()
+        public float GetFPS()
         {
-            return Client.Invoke("System.GetInfoLabels");
+            string[] label = new string[] {"System.FPS"};
+            JObject query = (JObject)Client.Invoke("System.GetInfoLabels", label);
+
+            if (query != null)
+                return (float)Convert.ToDouble(query["System.FPS"].Value<JValue>().Value);
+            else
+                return -1;
         }
 
-        public object GetInfoBooleans()
+        public string GetFreeMemory()
         {
-            return Client.Invoke("System.GetInfoBooleans");
-        } */
+            string[] label = new string[] {"System.FreeMemory"};
+            JObject query = (JObject)Client.Invoke("System.GetInfoLabels", label);
+
+            if (query != null)
+                return (string)query["System.FreeMemory"].Value<JValue>().Value;
+            else
+                return "";
+        }
+
+        //Will fix later
+        public Dictionary<string, string> GetInfoLabels(string[] labels)
+        {
+            JArray query = (JArray)Client.Invoke("System.GetInfoLabels", labels);
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            if (query != null)
+            {
+                foreach (JObject item in (JArray)query)
+                {
+                    list.Add("test1", "test2");
+                }
+            }
+
+            return list;
+        }
+
+        //Will fix later
+        public void GetInfoBooleans()
+        {
+            Client.Invoke("System.GetInfoBooleans");
+        }
 
         public void ShutDown()
         {
