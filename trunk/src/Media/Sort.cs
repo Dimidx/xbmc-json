8807a63985ab3f0f537a79579a469aa1e5bd79a8
johnsel <johnsel@d9727985-65ab-e424-f487-56934fdd5c17>
2010-08-28 16:33:49 +0000

    Added more Get*By* methods in AudioLibrary.cs Added Sort.cs

---
+++ trunk/src/Media/Sort.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace XbmcJson
+{
+    public class SortParams
+    {
+        private int Start = -1, Stop = -1;
+        private string Sortmethod, Sortorder;
+
+        public SortParams(string sortmethod, string sortorder, int start, int stop)
+        {
+            Sortmethod = sortmethod;
+            Sortorder = sortorder;
+            Start = start;
+            Stop = stop;
+        }
+
+        public JObject ToJObject()
+        {
+            JObject sortParams = new JObject();
+            if (this.Sortorder != null)
+                sortParams.Add(new JProperty("order", Sortorder));
+            if (this.Sortmethod != null)
+                sortParams.Add(new JProperty("method", Sortmethod));
+
+            JObject sortObj = new JObject();
+            if (this.Start != -1)
+                sortObj.Add(new JProperty("start", Start));
+            if (this.Stop != -1)
+                sortObj.Add(new JProperty("stop", Stop));
+
+            if (sortParams != null)
+                sortObj.Add(new JProperty("sort", sortParams));
+
+            return sortObj;
+        }
+    }
+}
