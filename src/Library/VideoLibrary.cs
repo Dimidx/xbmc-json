b5bd0a8f2c89336330c0ff53e924f062b41afcf5
johnsel <johnsel@d9727985-65ab-e424-f487-56934fdd5c17>
2010-08-28 17:22:42 +0000

    Updated VideoLibrary.cs; added SortParams + overloads too.

--- src/Library/VideoLibrary.cs
+++ src/Library/VideoLibrary.cs
@@ -7,42 +7,51 @@ namespace XbmcJson
 {
     public class XbmcVideoLibrary
     {
+        #region Declarations
         private JsonRpcClient Client;
         private string[] AllMovieFields = new string[] { "plot", "director", "writer", "studio", "genre", "year", "runtime", "rating", "tagline", "plotoutline" };
         private string[] AllTvShowFields = new string[] { "plot", "genre", "year", "rating" };
         private string[] AllSeasonFields = new string[] {"season", "genre", "year", "runtime", "rating" };
         private string[] AllEpisodeFields = new string[] { "season", "episode", "runtime", "year", "plot" };
         private string[] AllMusicVideoFields = new string[] { "title", "artist", "genre", "year", "rating", "album" };
+        #endregion
 
+        #region Constructor
         public XbmcVideoLibrary(JsonRpcClient client)
         {
             Client = client;
         }
+        #endregion
 
-        public List<Movie> GetMoviesAllFields(string sortMethod, string sortOrder, int? start, int? end)
+        #region GetMovies
+        public List<Movie> GetMoviesAllFields()
         {
-            return GetMovies(AllMovieFields, sortMethod, sortOrder, start, end);
+            return GetMovies(AllMovieFields, null);
+        }
+
+        public List<Movie> GetMoviesAllFields(SortParams sort)
+        {
+            return GetMovies(AllMovieFields, sort);
         }
 
         public List<Movie> GetMovies()
         {
-            return GetMovies(null, null, null, null, null);
+            return GetMovies(null, null);
         }
 
-        public List<Movie> GetMovies(string[] fields, string sortMethod, string sortOrder, int? start, int? end)
+        public List<Movie> GetMovies(string[] fields)
+        {
+            return GetMovies(fields, null);
+        }
+
+        public List<Movie> GetMovies(string[] fields, SortParams sort)
         {
             var args = new JObject();
 
             if (fields != null)
                 args.Add(new JProperty("fields", fields));
-            if (sortMethod != null)
-                args.Add(new JProperty("sortmethod", sortMethod));
-            if (sortOrder != null)
-                args.Add(new JProperty("sortorder", sortOrder));
-            if (start != null)
-                args.Add(new JProperty("start", start));
-            if (end != null)
-                args.Add(new JProperty("end", end));
+            if (sort != null)
+                args.Add(sort.ToJObject().Children());
 
             List<Movie> list = new List<Movie>();
             JObject query = (JObject)Client.Invoke("VideoLibrary.GetMovies", args);
@@ -57,32 +66,37 @@ namespace XbmcJson
 
             return list;
         }
+        #endregion
 
-        public List<TvShow> GetTvShowsAllFields(string sortMethod, string sortOrder, int? start, int? end)
+        #region GetTvShows
+        public List<TvShow> GetTvShowsAllFields()
         {
-            return GetTvShows(AllTvShowFields, sortMethod, sortOrder, start, end);
+            return GetTvShows(AllTvShowFields, null);
+        }
+
+        public List<TvShow> GetTvShowsAllFields(SortParams sort)
+        {
+            return GetTvShows(AllTvShowFields, sort);
         }
 
         public List<TvShow> GetTvShows()
         {
-            return GetTvShows(null, null, null, null, null);
+            return GetTvShows(null, null);
         }
 
+        public List<TvShow> GetTvShows(string[] fields)
+        {
+            return GetTvShows(fields, null);
+        }
 
-        public List<TvShow> GetTvShows(string[] fields, string sortMethod, string sortOrder, int? start, int? end)
+        public List<TvShow> GetTvShows(string[] fields, SortParams sort)
         {
             var args = new JObject();
 
             if (fields != null)
                 args.Add(new JProperty("fields", fields));
-            if (sortMethod != null)
-                args.Add(new JProperty("sortmethod", sortMethod));
-            if (sortOrder != null)
-                args.Add(new JProperty("sortorder", sortOrder));
-            if (start != null)
-                args.Add(new JProperty("start", start));
-            if (end != null)
-                args.Add(new JProperty("end", end));
+            if (sort != null)
+                args.Add(sort.ToJObject().Children());
 
 
             List<TvShow> list = new List<TvShow>();
@@ -98,33 +112,39 @@ namespace XbmcJson
 
             return list;
         }
+        #endregion
 
-        public List<Season> GetSeasonsAllFields(int tvShowId, string sortMethod, string sortOrder, int? start, int? end)
+        #region GetSeasons
+        public List<Season> GetSeasonsAllFields(int tvShowId)
         {
-            return GetSeasons(tvShowId, AllSeasonFields, sortMethod, sortOrder, start, end);
+            return GetSeasons(tvShowId, AllSeasonFields, null);
+        }
+
+        public List<Season> GetSeasonsAllFields(int tvShowId, SortParams sort)
+        {
+            return GetSeasons(tvShowId, AllSeasonFields, sort);
         }
 
         public List<Season> GetSeasons(int tvShowId)
         {
-            return GetSeasons(tvShowId, null, null, null, null, null);
+            return GetSeasons(tvShowId, null, null);
         }
 
-        public List<Season> GetSeasons(int tvShowId, string[] fields, string sortMethod, string sortOrder, int? start, int? end)
+        public List<Season> GetSeasons(int tvShowId, string[] fields)
+        {
+            return GetSeasons(tvShowId, fields, null);
+        }
+
+        public List<Season> GetSeasons(int? tvShowId, string[] fields, SortParams sort)
         {
             var args = new JObject();
 
-            args.Add(new JProperty("tvshowid", tvShowId));
-
+            if (tvShowId != null)
+                args.Add(new JProperty("tvshowid", tvShowId));
             if (fields != null)
                 args.Add(new JProperty("fields", fields));
-            if (sortMethod != null)
-                args.Add(new JProperty("sortmethod", sortMethod));
-            if (sortOrder != null)
-                args.Add(new JProperty("sortorder", sortOrder));
-            if (start != null)
-                args.Add(new JProperty("start", start));
-            if (end != null)
-                args.Add(new JProperty("end", end));
+            if (sort != null)
+                args.Add(sort.ToJObject().Children());
 
             List<Season> list = new List<Season>();
             JObject query = (JObject)Client.Invoke("VideoLibrary.GetSeasons", args);
@@ -139,34 +159,42 @@ namespace XbmcJson
 
             return list;
         }
+        #endregion
 
-        public List<Episode> GetEpisodesAllFields(int tvShowId, int season, string sortMethod, string sortOrder, int? start, int? end)
+        #region GetEpisodes
+        public List<Episode> GetEpisodesAllFields(int tvShowId, int season)
         {
-            return GetEpisodes(tvShowId, season, AllEpisodeFields, sortMethod, sortOrder, start, end);
+            return GetEpisodes(tvShowId, season, AllEpisodeFields, null);
+        }
+
+        public List<Episode> GetEpisodesAllFields(int tvShowId, int season, SortParams sort)
+        {
+            return GetEpisodes(tvShowId, season, AllEpisodeFields, sort);
         }
 
         public List<Episode> GetEpisodes(int tvShowId, int season)
         {
-            return GetEpisodes(tvShowId, season, null, null, null, null, null);
+            return GetEpisodes(tvShowId, season, null, null);
         }
 
-        public List<Episode> GetEpisodes(int tvShowId, int season, string[] fields, string sortMethod, string sortOrder, int? start, int? end)
+        public List<Episode> GetEpisodes(int tvShowId, int season, string[] fields)
+        {
+            return GetEpisodes(tvShowId, season, fields, null);
+        }
+
+        public List<Episode> GetEpisodes(int? tvShowId, int? season, string[] fields, SortParams sort)
         {
             var args = new JObject();
 
-            args.Add(new JProperty("tvshowid", tvShowId));
-            args.Add(new JProperty("season", season));
+            if (tvShowId != null)
+                args.Add(new JProperty("tvshowid", tvShowId));
+            if (season != null)
+                args.Add(new JProperty("season", season));
 
             if (fields != null)
                 args.Add(new JProperty("fields", fields));
-            if (sortMethod != null)
-                args.Add(new JProperty("sortmethod", sortMethod));
-            if (sortOrder != null)
-                args.Add(new JProperty("sortorder", sortOrder));
-            if (start != null)
-                args.Add(new JProperty("start", start));
-            if (end != null)
-                args.Add(new JProperty("end", end));
+            if (sort != null)
+                args.Add(sort.ToJObject().Children());
 
             List<Episode> list = new List<Episode>();
             JObject query = (JObject)Client.Invoke("VideoLibrary.GetEpisodes", args);
@@ -181,22 +209,37 @@ namespace XbmcJson
 
             return list;
         }
+        #endregion
 
+        #region GetMusicVideos
         public List<MusicVideo> GetMusicVideosAllFields()
         {
             return GetMusicVideos(AllMusicVideoFields);
         }
 
+        public List<MusicVideo> GetMusicVideosAllFields(SortParams sort)
+        {
+            return GetMusicVideos(AllMusicVideoFields, sort);
+        }
+
         public List<MusicVideo> GetMusicVideos()
         {
-            return GetMusicVideos(null);
+            return GetMusicVideos(null, null);
         }
+
         public List<MusicVideo> GetMusicVideos(string[] fields)
+        {
+            return GetMusicVideos(fields, null);
+        }
+
+        public List<MusicVideo> GetMusicVideos(string[] fields, SortParams sort)
         {
             var args = new JObject();
 
             if (fields != null)
                 args.Add(new JProperty("fields", fields));
+            if (sort != null)
+                args.Add(sort.ToJObject().Children());
 
             JObject query = (JObject)Client.Invoke("VideoLibrary.GetMusicVideos", args);
             List<MusicVideo> list = new List<MusicVideo>();
@@ -211,31 +254,32 @@ namespace XbmcJson
 
             return list;
         }
+#endregion
 
-        public List<Movie> GetRecentlyAddedMoviesAllFields(string sortMethod, string sortOrder, int? start, int? end)
+        #region GetRecentlyAddedMovies
+        public List<Movie> GetRecentlyAddedMoviesAllFields()
         {
-            return GetMovies(AllMovieFields, sortMethod, sortOrder, start, end);
+            return GetRecentlyAddedMovies(AllMovieFields, null);
         }
 
-        public List<Movie> GetRecentlyAddedMovies()
+        public List<Movie> GetRecentlyAddedMoviesAllFields(SortParams sort)
         {
-            return GetRecentlyAddedMovies(null, null, null, null, null);
+            return GetRecentlyAddedMovies(AllMovieFields, sort);
         }
 
-        public List<Movie> GetRecentlyAddedMovies(string[] fields, string sortMethod, string sortOrder, int? start, int? end)
+        public List<Movie> GetRecentlyAddedMovies(string[] fields)
+        {
+            return GetRecentlyAddedMovies(fields, null);
+        }
+
+        public List<Movie> GetRecentlyAddedMovies(string[] fields, SortParams sort)
         {
             var args = new JObject();
 
             if (fields != null)
                 args.Add(new JProperty("fields", fields));
-            if (sortMethod != null)
-                args.Add(new JProperty("sortmethod", sortMethod));
-            if (sortOrder != null)
-                args.Add(new JProperty("sortorder", sortOrder));
-            if (start != null)
-                args.Add(new JProperty("start", start));
-            if (end != null)
-                args.Add(new JProperty("end", end));
+            if (sort != null)
+                args.Add(sort.ToJObject().Children());
 
             List<Movie> list = new List<Movie>();
             JObject query = (JObject)Client.Invoke("VideoLibrary.GetRecentlyAddedMovies", args);
@@ -250,31 +294,32 @@ namespace XbmcJson
 
             return list;
         }
+        #endregion
 
-        public List<Episode> GetRecentlyAddedEpisodesAllFields(string sortMethod, string sortOrder, int? start, int? end)
+        #region GetRecentlyAddedEpisodes
+        public List<Episode> GetRecentlyAddedEpisodesAllFields()
         {
-            return GetRecentlyAddedEpisodes(AllEpisodeFields, sortMethod, sortOrder, start, end);
+            return GetRecentlyAddedEpisodes(AllEpisodeFields, null);
         }
 
-        public List<Episode> GetRecentlyAddedEpisodes()
+        public List<Episode> GetRecentlyAddedEpisodesAllFields(SortParams sort)
         {
-            return GetRecentlyAddedEpisodes(null, null, null, null, null);
+            return GetRecentlyAddedEpisodes(AllEpisodeFields, sort);
         }
 
-        public List<Episode> GetRecentlyAddedEpisodes(string[] fields, string sortMethod, string sortOrder, int? start, int? end)
+        public List<Episode> GetRecentlyAddedEpisodes(string[] fields)
+        {
+            return GetRecentlyAddedEpisodes(fields, null);
+        }
+
+        public List<Episode> GetRecentlyAddedEpisodes(string[] fields, SortParams sort)
         {
             var args = new JObject();
 
             if (fields != null)
                 args.Add(new JProperty("fields", fields));
-            if (sortMethod != null)
-                args.Add(new JProperty("sortmethod", sortMethod));
-            if (sortOrder != null)
-                args.Add(new JProperty("sortorder", sortOrder));
-            if (start != null)
-                args.Add(new JProperty("start", start));
-            if (end != null)
-                args.Add(new JProperty("end", end));
+            if (sort != null)
+                args.Add(sort.ToJObject().Children());
 
             List<Episode> list = new List<Episode>();
             JObject query = (JObject)Client.Invoke("VideoLibrary.GetRecentlyAddedEpisodes", args);
@@ -289,31 +334,37 @@ namespace XbmcJson
 
             return list;
         }
+        #endregion
 
-        public List<MusicVideo> GetRecentlyAddedMusicVideosAllFields(string sortMethod, string sortOrder, int? start, int? end)
+        #region GetRecentlyAddedMusic
+        public List<MusicVideo> GetRecentlyAddedMusicVideosAllFields()
         {
-            return GetRecentlyAddedMusicVideos(AllMusicVideoFields, sortMethod, sortOrder, start, end);
+            return GetRecentlyAddedMusicVideos(AllMusicVideoFields, null);
+        }
+
+        public List<MusicVideo> GetRecentlyAddedMusicVideosAllFields(SortParams sort)
+        {
+            return GetRecentlyAddedMusicVideos(AllMusicVideoFields, sort);
         }
 
         public List<MusicVideo> GetRecentlyAddedMusicVideos()
         {
-            return GetRecentlyAddedMusicVideos(null, null, null, null, null);
+            return GetRecentlyAddedMusicVideos(null, null);
         }
 
-        public List<MusicVideo> GetRecentlyAddedMusicVideos(string[] fields, string sortMethod, string sortOrder, int? start, int? end)
+        public List<MusicVideo> GetRecentlyAddedMusicVideos(string[] fields)
+        {
+            return GetRecentlyAddedMusicVideos(fields, null);
+        }
+
+        public List<MusicVideo> GetRecentlyAddedMusicVideos(string[] fields, SortParams sort)
         {
             var args = new JObject();
 
             if (fields != null)
                 args.Add(new JProperty("fields", fields));
-            if (sortMethod != null)
-                args.Add(new JProperty("sortmethod", sortMethod));
-            if (sortOrder != null)
-                args.Add(new JProperty("sortorder", sortOrder));
-            if (start != null)
-                args.Add(new JProperty("start", start));
-            if (end != null)
-                args.Add(new JProperty("end", end));
+            if (sort != null)
+                args.Add(sort.ToJObject().Children());
 
             List<MusicVideo> list = new List<MusicVideo>();
             JObject query = (JObject)Client.Invoke("VideoLibrary.GetRecentlyAddedMusicVideos", args);
@@ -328,10 +379,13 @@ namespace XbmcJson
 
             return list;
         }
+        #endregion
 
+        #region ScanForContent
         public void ScanForContent()
         {
             Client.Invoke("VideoLibrary.ScanForContent");
         }
+        #endregion
     }
 }
